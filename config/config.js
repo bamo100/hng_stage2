9b640690ab983ddeb53e83951cdc63cd8095704c
bamo100 <bankolediamond10@gmail.com>
2024-07-09 16:52:34 +0100

    2nd changes

--- config/config.js
+++ config/config.js
@@ -6,14 +6,16 @@ module.exports =
       "username": process.env.DEV_DB_USERNAME,
       "password": process.env.DEV_DB_PASSWORD,
       "database": process.env.DEV_DB_DATABASE,
-      "host": process.env.PROD_DB_HOST || '127.0.0.1',
+      "host": process.env.DEV_DB_HOST || '127.0.0.1',
+      "port": process.env.DEV_DB_PORT,
       "dialect": "postgres"
     },
     "test": {
       "username": process.env.TEST_DB_USERNAME,
       "password": process.env.TEST_DB_PASSWORD,
       "database": process.env.TEST_DB_DATABASE,
-      "host": process.env.PROD_DB_HOST || '127.0.0.1',
+      "host": process.env.TEST_DB_HOST || '127.0.0.1',
+      "port": process.env.TEST_DB_PORT,
       "dialect": "postgres"
     },
     "production": {
@@ -21,6 +23,7 @@ module.exports =
       "password": process.env.PROD_DB_PASSWORD,
       "database": process.env.PROD_DB_DATABASE,
       "host": process.env.PROD_DB_HOST || '127.0.0.1',
+      "port": process.env.PROD_DB_PORT,
       "dialect": "postgres"
     }
   }
--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const config = require('../config/config.js')[env];
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
+  port: config.port,
   dialect: config.dialect
 });
 
