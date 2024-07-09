521026f4e40a4695c6f1aabf8bd2e1d1fc2e6920
bamo100 <bankolediamond10@gmail.com>
2024-07-09 16:42:27 +0100

    changes

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const userRoutes = require('./routes/user');
 const organizationRoutes = require('./routes/organization');
 const dotenv = require('dotenv')
 dotenv.config()
-// const db = require('./models')
-// const bcrypt = require('bcryptjs')
-// const jwt = require('jsonwebtoken')
-// const jwtsecret = process.env.JWT_SECRET
+
 
 const app = express()
 
@@ -17,15 +14,6 @@ app.use(bodyParser.json())
 // User routes
 app.use('/auth', userRoutes);
 app.use('/api', organizationRoutes);
-
-// db.sequelize.sync()
-//     .then(() => console.log('Database synced'))
-//     .catch((error) => console.error('Error syncing database:', error))
-
-
-  
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 const PORT = process.env.PORT || 3000;
 
