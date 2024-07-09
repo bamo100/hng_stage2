8e53a210d1286ab50936f9b69fdce4138c84b148
bamo100 <bankolediamond10@gmail.com>
2024-07-09 21:35:51 +0100

    add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { sequelize } = require('./models');
 const userRoutes = require('./routes/user');
 const organizationRoutes = require('./routes/organization');
 const dotenv = require('dotenv')
+const { Sequelize } = require('sequelize');
 dotenv.config()
 
 
@@ -14,6 +15,40 @@ app.use(bodyParser.json())
 // User routes
 app.use('/auth', userRoutes);
 app.use('/api', organizationRoutes);
+
+app.get('/debug', async (req, res) => {
+    const pgInstalled = !!require.resolve('pg');
+    const sequelize = new Sequelize(process.env.PROD_DB_NAME, process.env.PROD_DB_USER, process.env.PROD_DB_PASSWORD, {
+      host: process.env.PROD_DB_HOST,
+      port: process.env.PROD_DB_PORT,
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    });
+  
+    try {
+      await sequelize.authenticate();
+      res.send({
+        message: 'Connection has been established successfully.',
+        pgInstalled,
+        nodeVersion: process.version,
+        npmVersion: require('child_process').execSync('npm -v').toString().trim(),
+      });
+    } catch (error) {
+      res.status(500).send({
+        message: 'Unable to connect to the database',
+        error: error.message,
+        pgInstalled,
+        nodeVersion: process.version,
+        npmVersion: require('child_process').execSync('npm -v').toString().trim(),
+      });
+    }
+  });
+
 
 const PORT = process.env.PORT || 3000;
 
